7fbffdb1a747e700777575e6b6f48c3f82e543d2
SEIYA <skkasans@gmail.com>
2020-06-28 19:33:43 +0900

    メールクラスの作成

---
+++ app/Mail/ContactMail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class ContactMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+
+     protected $contact;
+
+    public function __construct($contact)
+    {
+        //
+        $this->contact = $contact;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject('テスト送信')
+                    ->from('hoge@hoge.com')
+                    ->view('contact.mail')
+                    ->with(['contact' => $this->contact]);
+    }
+}
